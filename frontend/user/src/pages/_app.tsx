168d93ed52325f8f928c7ad0acb2262b4350bbe1
Shota Taniguchi <staniguchi@genvidtech.com>
2023-09-30 21:19:45 +0900

    chore - added eslint.workingDirectories in vscode setting.json

--- frontend/user/src/pages/_app.tsx
+++ frontend/user/src/pages/_app.tsx
@@ -1,6 +1,5 @@
-import '@styles/Home.module.css';
-
 import type { AppProps } from 'next/app';
+import '@styles/Home.module.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line react/jsx-props-no-spreading
