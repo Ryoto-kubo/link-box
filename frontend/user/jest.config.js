6df04e6c7b5822e46e721f44eb5a5a8735485b73
Shota Taniguchi <staniguchi@genvidtech.com>
2023-09-30 18:09:56 +0900

    feat - install jest

---
+++ frontend/user/jest.config.js
@@ -0,0 +1,13 @@
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
+};
+
+export default createJestConfig(customJestConfig);
