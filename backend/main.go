de0229dd58900ce089ee2b7598ac708a3f6213cd
ryoto kubo <sax.guiter@icloud.com>
2023-10-01 21:39:28 +0900

    fix: setup connect db on local

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,8 @@ import (
 	"embed"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 //go:embed resources/config/application.*.yml
@@ -12,6 +14,15 @@ var yamlFile embed.FS
 //go:embed resources/config/zaplogger.*.yml
 var zapYamlFile embed.FS
 
+type User struct {
+	gorm.Model
+	Id  int `gorm:"primaryKey" json:"id"`
+	Name string `json:"name"`
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
+
+
 func main() {
 	// conf, env := config.LoadAppConfig(yamlFile)
 	// logger := logger.InitLogger(env, zapYamlFile)
@@ -19,11 +30,23 @@ func main() {
 
 	// rep := repository.NewLinkBoxRepository(logger, conf)
 
+	dsn := "testuser:password@tcp(link-box-mysql:3306)/link-box?charset=utf8&parseTime=true"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
+	})
+	r.GET("/users", func(c *gin.Context) {
+		var users []User
+		db.Unscoped().Find(&users)
+		c.JSON(200, users)
 	})
 
 	r.Run(":8302")
