881dcf36a2f5f08933186393b49f1ece3888bb3d
ryoto kubo <sax.guiter@icloud.com>
2023-10-12 22:05:16 +0900

    fix: setup migration

--- database/migrations/000001_create_users_table.up.sql
+++ database/migrations/000001_create_users_table.up.sql
@@ -4,3 +4,10 @@ CREATE TABLE `users` (
   `email` VARCHAR(255) NOT NULL UNIQUE,
   `password` VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE `test_users` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `user_name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL UNIQUE,
+  `password` VARCHAR(255) NOT NULL
+);
