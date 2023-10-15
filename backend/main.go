b8af2a461e64dc6872596a81489004010a148dcb
ryoto kubo <sax.guiter@icloud.com>
2023-10-15 19:30:10 +0900

    test

--- backend/main.go
+++ backend/main.go
@@ -46,7 +46,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"hoge": mustGet("MYSQL_HOST"),
+			"next": mustGet("MYSQL_HOST"),
 		})
 	})
 	r.GET("/users", func(c *gin.Context) {
